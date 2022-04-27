72ca218bea8a4b167bb8ad6723f40b244cb78744
Steve Meyfroidt <steve.meyfroidt@gmail.com>
2022-04-27 19:41:21 +0100

    Make print-res canvas

--- src/globals.h
+++ src/globals.h
@@ -3,8 +3,8 @@
 
 namespace Constants {
   // Note that camera captures 640x480 not 720x480
-  const size_t canvasWidth = 72*36;
-  const size_t canvasHeight = 72*24;
+  const size_t canvasWidth = 240*36;
+  const size_t canvasHeight = 240*24;
   const size_t resizedVideoWidth = 720;
   const size_t resizedVideoHeight = 480;
   const size_t videoGrabWidth = 640;
